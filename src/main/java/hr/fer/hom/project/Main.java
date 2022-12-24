30e000dadef15752f2c236164a0fe1a534a316a8
Matej Cubek <matej.cubek@gmail.com>
2022-12-25 00:14:19 +0100

    init

---
+++ src/main/java/hr/fer/hom/project/Main.java
@@ -0,0 +1,12 @@
+package hr.fer.hom.project;
+
+/**
+ * @author matejc
+ * Created on 25.12.2022.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
